67095c32a097b60b6bd4c3eb340c27ca216b2326
Robin <robin.herrmann@alumni.fh-aachen.de>
2025-07-14 16:52:21 +0200

    Latex config mit texlab und vimtex

--- init.lua
+++ init.lua
@@ -249,6 +249,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   'Olical/conjure',
+  'lervag/vimtex',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -673,6 +674,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         clojure_lsp = {},
+        texlab = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
