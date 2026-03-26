07c638139515a166011ca958b2289787c9c1736b
Robin <robin.herrmann@alumni.fh-aachen.de>
2026-03-26 18:54:49 +0100

    Rose Pine Require und nvim-java Setup+Require und jdtls Setup aber ohne require, weil das mit nvim-java nicht zusammen geht

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -762,7 +762,9 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-  require 'custom.rose-pine-install'
+  require 'custom.rose-pine-install',
+  -- require 'custom.lsp-jdtls',
+  require 'custom.nvim-java'
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
